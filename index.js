f2dbc59609cfb0a01715ccde96631ccf1fc1ee27
unknown <muktinathrajbanshi786@gmail.com>
2026-03-20 11:05:45 +0545

    little bit ui/ux tackcle

--- index.js
+++ index.js
@@ -147,6 +147,12 @@ function updateChart() {
 
 document.getElementById("darkToggle").onclick = function () {
   document.body.classList.toggle("dark");
+
+   if (document.body.classList.contains("dark")) {
+    this.textContent = "☀️ Light Mode";
+  } else {
+    this.textContent = "🌙 Dark Mode";
+  }
 };
 
 render();
